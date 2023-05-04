90f9513ed307d195a2ae474151cfb78a7061265d
Stepanova Diana <stepanova.diana26092002@gmail.com>
2023-05-04 21:41:51 +0300

    servlet

---
+++ src/main/java/servlet/TestServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet("/")
+
+public class TestServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        res.setContentType("text/html");
+        try(var writer=res.getWriter()){
+            writer.write("hello from me");
+        }
+    }
+    @Override
+    public void destroy(){
+        super.destroy();
+    }
+}
